418088460393a1df2db6ecf0748dd83421a57628
Gabriel Lima <73233318+GabrielLima60@users.noreply.github.com>
2024-04-01 17:24:40 -0300

    Add files via upload

--- code/program_plot.py
+++ code/program_plot.py
@@ -1,3 +1,7 @@
+'''
+    This file creates a plot of the the results after the analysis
+'''
+
 import sys
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg
@@ -6,17 +10,26 @@ import pandas as pd
 from PIL import Image
 
 def plot():
+    '''
+        Essa é a fun
+    '''
     dataset = pd.read_csv('results table\\results.csv')
 
     metrics = ["f1_score", "processing_time", "memory_usage"]
     titles = ["F1 Score Comparison", "Processing Time Comparison (seconds)", "Memory Usage Comparison (KiB)"]
 
-    fig, axes = plt.subplots(len(metrics), 1, figsize=(10, 5 * len(metrics)), dpi=120)
+    _, axes = plt.subplots(len(metrics), 1, figsize=(10, 5 * len(metrics)), dpi=120)
 
     sns.set_palette("hls")
 
     for i, metric in enumerate(metrics):
-        ax = sns.barplot(data=dataset, x="model", y=metric, hue="technique", errorbar=None, ax=axes[i])
+        ax = sns.barplot(data=dataset, \
+                         x="model", \
+                         y=metric, \
+                         hue="technique", \
+                         errorbar=None, \
+                         ax=axes[i])
+
         axes[i].set_xlabel("AI Model")
         axes[i].set_ylabel(metric)
         axes[i].set_title(titles[i])
@@ -25,14 +38,14 @@ def plot():
 
         for p in ax.patches:
             height = p.get_height() if not pd.isnull(p.get_height()) else 0
-            if height != 0:  
+            if height != 0:
                 ax.annotate(f'{height:.2f}', (p.get_x() + p.get_width() / 2., height),
                             ha='center', va='center', fontsize=8, color='black', xytext=(0, 5),
                             textcoords='offset points')
 
     plt.tight_layout()
 
-    figure = plt.gcf()  
+    figure = plt.gcf()
     canvas = FigureCanvasAgg(figure)
     canvas.draw()
     image_data = canvas.tostring_rgb()
